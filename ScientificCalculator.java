771050d2622bcd9b8e2b70bdbe50a127a78fd379
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2020-09-11 05:24:20 +0600

    Create ScientificCalculator.java

---
+++ ScientificCalculator.java
@@ -0,0 +1,8 @@
+package ScientificCalculator;
+
+public class ScientificCalculator {
+    public static void main(String[] args) {
+        CalculatorUI scientific = new CalculatorUI();
+        scientific.setVisible(true);
+    }
+}
